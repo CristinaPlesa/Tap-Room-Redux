1ddc07f12b0772f99d0d3341b3951261c194afb4
Cristina Plesa <cristinaplesa8@gmail.com>
2021-07-02 11:54:48 -0700

    add div style for background image

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,9 @@ import breweryImage from "./../img/Tap-Room-Brewery.jpg";
 function Header(){
   return (
     <React.Fragment>
+     <div style={{ backgroundImage: `ur(${breweryImage})`}}>
       <h1>Help Queue</h1>
-      <style>
-  
-      </style>
-      <img width="50%" src={breweryImage} alt="Brewery machines in a warehouse" />
+     </div>
     </React.Fragment>
   );
 }
