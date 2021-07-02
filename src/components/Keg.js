5f12e50d47385a57249631ee28657ba8a8959811
Cristina Plesa <cristinaplesa8@gmail.com>
2021-07-02 16:08:37 -0700

    refactor Keg js to only display name and price

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -5,9 +5,7 @@ function Keg(props){
   return (
     <React.Fragment>
       <div onClick = {() => props.whenKegClicked(props.id)}>
-        <h3><span class="Detail-link">{props.name} - {props.brand}</span></h3>
-        <p><em>{props.price}</em></p>
-        <p><em>{props.alcoholContent}</em></p>
+        <h3><span class="Detail-link">{props.name} - {props.price}</span></h3>
         <hr/>
       </div>
     </React.Fragment>
--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -6,14 +6,12 @@ function KegDetail(props){
 
   return (
     <React.Fragment>
-      <div class="Detail-page">
-        <h1>Keg Detail</h1>
-        <h3>{keg.name}</h3>
-        <p><em>Brand: {keg.brand}</em></p>
-        <p><em>Price per Pint: {keg.price}</em></p>
-        <p><em>Alcohol Content: {keg.alcoholContent}</em></p>
-        <p><em>Pints left over: </em></p>
-      </div>
+      <h1>Keg Detail</h1>
+      <h3>{keg.name}</h3>
+      <p><em>Brand: {keg.brand}</em></p>
+      <p><em>Price per Pint: {keg.price}</em></p>
+      <p><em>Alcohol Content: {keg.alcoholContent}</em></p>
+      <p><em>Pints left over: </em></p>
       <hr/>
     </React.Fragment>
   );
